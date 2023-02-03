816a5d5d02d6cedb4fc6f319264020908b55fc2c
PiensaWeb <104882130+PiensaWeb@users.noreply.github.com>
2023-02-03 02:38:54 -0600

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,120 @@
+if(document.querySelector('#container-slider')){
+    setInterval('fntExecuteSlide("next")',9000);
+ }
+ //------------------------------ LIST SLIDER -------------------------
+ if(document.querySelector('.listslider')){
+    let link = document.querySelectorAll(".listslider li a");
+    link.forEach(function(link) {
+       link.addEventListener('click', function(e){
+          e.preventDefault();
+          let item = this.getAttribute('itlist');
+          let arrItem = item.split("_");
+          fntExecuteSlide(arrItem[1]);
+          return false;
+       });
+     });
+ }
+ 
+ function fntExecuteSlide(side){
+     let parentTarget = document.getElementById('slider');
+     let elements = parentTarget.getElementsByTagName('li');
+     let curElement, nextElement;
+ 
+     for(var i=0; i<elements.length;i++){
+ 
+         if(elements[i].style.opacity==1){
+             curElement = i;
+             break;
+         }
+     }
+     if(side == 'prev' || side == 'next'){
+ 
+         if(side=="prev"){
+             nextElement = (curElement == 0)?elements.length -1:curElement -1;
+         }else{
+             nextElement = (curElement == elements.length -1)?0:curElement +1;
+         }
+     }else{
+         nextElement = side;
+         side = (curElement > nextElement)?'prev':'next';
+ 
+     }
+     //RESALTA LOS PUNTOS
+     let elementSel = document.getElementsByClassName("listslider")[0].getElementsByTagName("a");
+     elementSel[curElement].classList.remove("item-select-slid");
+     elementSel[nextElement].classList.add("item-select-slid");
+     elements[curElement].style.opacity=0;
+     elements[curElement].style.zIndex =0;
+     elements[nextElement].style.opacity=1;
+     elements[nextElement].style.zIndex =1;
+ }
+
+ 
+
+ $(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+const one = document.querySelector(".one");
+const two = document.querySelector(".two");
+const three = document.querySelector(".three");
+const four = document.querySelector(".four");
+const five = document.querySelector(".five");
+
+one.onclick = function() {
+    one.classList.add("active");
+    two.classList.remove("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
+    five.classList.remove("active");
+}
+
+two.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.remove("active");
+    four.classList.remove("active");
+    five.classList.remove("active");
+}
+three.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.add("active");
+    four.classList.remove("active");
+    five.classList.remove("active");
+}
+four.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.add("active");
+    four.classList.add("active");
+    five.classList.remove("active");
+}
+five.onclick = function() {
+    one.classList.add("active");
+    two.classList.add("active");
+    three.classList.add("active");
+    four.classList.add("active");
+    five.classList.add("active");
+}
+
